e721e9a256187e1c7ad99ee5dc86ef03973439f1
CalvinYeung <calyeung92@gmail.com>
2015-07-20 14:18:55 -0400

    emails working on signup

--- app/controllers/users/users.authentication.server.controller.js
+++ app/controllers/users/users.authentication.server.controller.js
@@ -3,22 +3,37 @@
 /**
  * Module dependencies.
  */
-var _ = require('lodash'),
-	errorHandler = require('../errors.server.controller'),
-	mongoose = require('mongoose'),
-	passport = require('passport'),
-	User = mongoose.model('User');
+ var _ = require('lodash'),
+ errorHandler = require('../errors.server.controller'),
+ mongoose = require('mongoose'),
+ passport = require('passport'),
+ User = mongoose.model('User');
+
+ var nodeMailer = require('nodemailer');
+ var transporter = nodeMailer.createTransport({
+ 	service: 'Gmail',
+ 	auth: {
+ 		user: 'testangular2015@gmail.com',
+ 		pass: 'tester12345'
+ 	}
+ });
 
 /**
  * Signup
  */
-exports.signup = function(req, res) {
+ exports.signup = function(req, res) {
 	// For security measurement we remove the roles from the req.body object
 	delete req.body.roles;
 
 	// Init Variables
 	var user = new User(req.body);
 	var message = null;
+	var mailOptions = {
+		from: 'Remax',
+		to: user.email,
+		subject: 'welcome to Remax!',
+		text: 'Thank you for signing up! Your new username is : ' + user.username + '.'
+	};
 
 	// Add missing user fields
 	user.provider = 'local';
@@ -40,6 +55,14 @@ exports.signup = function(req, res) {
 					res.status(400).send(err);
 				} else {
 					res.json(user);
+
+					transporter.sendMail(mailOptions, function(error, info){
+						if(error){
+							return console.log(error);
+						}
+						console.log('Message sent: ' + info.response);
+
+					});
 				}
 			});
 		}
@@ -49,11 +72,11 @@ exports.signup = function(req, res) {
 /**
  * Signin after passport authentication
  */
-exports.signin = function(req, res, next) {
-	passport.authenticate('local', function(err, user, info) {
-		if (err || !user) {
-			res.status(400).send(info);
-		} else {
+ exports.signin = function(req, res, next) {
+ 	passport.authenticate('local', function(err, user, info) {
+ 		if (err || !user) {
+ 			res.status(400).send(info);
+ 		} else {
 			// Remove sensitive data before login
 			user.password = undefined;
 			user.salt = undefined;
@@ -72,36 +95,36 @@ exports.signin = function(req, res, next) {
 /**
  * Signout
  */
-exports.signout = function(req, res) {
-	req.logout();
-	res.redirect('/');
-};
+ exports.signout = function(req, res) {
+ 	req.logout();
+ 	res.redirect('/');
+ };
 
 /**
  * OAuth callback
  */
-exports.oauthCallback = function(strategy) {
-	return function(req, res, next) {
-		passport.authenticate(strategy, function(err, user, redirectURL) {
-			if (err || !user) {
-				return res.redirect('/#!/signin');
-			}
-			req.login(user, function(err) {
-				if (err) {
-					return res.redirect('/#!/signin');
-				}
+ exports.oauthCallback = function(strategy) {
+ 	return function(req, res, next) {
+ 		passport.authenticate(strategy, function(err, user, redirectURL) {
+ 			if (err || !user) {
+ 				return res.redirect('/#!/signin');
+ 			}
+ 			req.login(user, function(err) {
+ 				if (err) {
+ 					return res.redirect('/#!/signin');
+ 				}
 
-				return res.redirect(redirectURL || '/');
-			});
-		})(req, res, next);
-	};
-};
+ 				return res.redirect(redirectURL || '/');
+ 			});
+ 		})(req, res, next);
+ 	};
+ };
 
 /**
  * Helper function to save or update a OAuth user profile
  */
-exports.saveOAuthUserProfile = function(req, providerUserProfile, done) {
-	if (!req.user) {
+ exports.saveOAuthUserProfile = function(req, providerUserProfile, done) {
+ 	if (!req.user) {
 		// Define a search query fields
 		var searchMainProviderIdentifierField = 'providerData.' + providerUserProfile.providerIdentifierField;
 		var searchAdditionalProviderIdentifierField = 'additionalProvidersData.' + providerUserProfile.provider + '.' + providerUserProfile.providerIdentifierField;
@@ -174,11 +197,11 @@ exports.saveOAuthUserProfile = function(req, providerUserProfile, done) {
 /**
  * Remove OAuth provider
  */
-exports.removeOAuthProvider = function(req, res, next) {
-	var user = req.user;
-	var provider = req.param('provider');
+ exports.removeOAuthProvider = function(req, res, next) {
+ 	var user = req.user;
+ 	var provider = req.param('provider');
 
-	if (user && provider) {
+ 	if (user && provider) {
 		// Delete the additional provider
 		if (user.additionalProvidersData[provider]) {
 			delete user.additionalProvidersData[provider];
