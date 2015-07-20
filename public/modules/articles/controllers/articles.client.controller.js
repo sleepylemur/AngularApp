f1678c9b83948a79571fee8866a8335183136bed
Meir <meir@Meirs-Mac.local>
2015-07-20 13:22:46 -0400

    new angular app with working terms modal

---
+++ public/modules/articles/controllers/articles.client.controller.js
@@ -0,0 +1,110 @@
+'use strict';
+
+angular.module('articles').controller('ArticlesController', ['$scope', '$stateParams', '$location', 'Authentication', 'Articles',
+	function($scope, $stateParams, $location, Authentication, Articles) {
+		$scope.authentication = Authentication;
+
+		$scope.create = function() {
+			var article = new Articles({
+				title: this.title,
+				content: this.content
+			});
+			article.$save(function(response) {
+				$location.path('articles/' + response._id);
+
+				$scope.title = '';
+				$scope.content = '';
+			}, function(errorResponse) {
+				$scope.error = errorResponse.data.message;
+			});
+		};
+
+		$scope.remove = function(article) {
+			if (article) {
+				article.$remove();
+
+				for (var i in $scope.articles) {
+					if ($scope.articles[i] === article) {
+						$scope.articles.splice(i, 1);
+					}
+				}
+			} else {
+				$scope.article.$remove(function() {
+					$location.path('articles');
+				});
+			}
+		};
+
+		$scope.update = function() {
+			var article = $scope.article;
+
+			article.$update(function() {
+				$location.path('articles/' + article._id);
+			}, function(errorResponse) {
+				$scope.error = errorResponse.data.message;
+			});
+		};
+
+		$scope.find = function() {
+			$scope.articles = Articles.query();
+		};
+
+		$scope.findOne = function() {
+			$scope.article = Articles.get({
+				articleId: $stateParams.articleId
+			});
+		};
+	}
+]);
+
+angular.module('articles').controller('ModalDemoCtrl', function ($scope, $modal, $log) {
+
+  $scope.items = ['item1', 'item2', 'item3'];
+
+  $scope.animationsEnabled = true;
+
+  $scope.open = function (size) {
+
+    var modalInstance = $modal.open({
+      animation: $scope.animationsEnabled,
+      templateUrl: 'myModalContent.html',
+      controller: 'ModalInstanceCtrl',
+      size: size,
+      resolve: {
+        items: function () {
+          return $scope.items;
+        }
+      }
+    });
+
+    modalInstance.result.then(function (selectedItem) {
+      $scope.selected = selectedItem;
+    }, function () {
+      $log.info('Modal dismissed at: ' + new Date());
+    });
+  };
+
+  $scope.toggleAnimation = function () {
+    $scope.animationsEnabled = !$scope.animationsEnabled;
+  };
+
+});
+
+// Please note that $modalInstance represents a modal window (instance) dependency.
+// It is not the same as the $modal service used above.
+
+angular.module('articles').controller('ModalInstanceCtrl', function ($scope, $modalInstance, items) {
+
+  $scope.items = items;
+  $scope.selected = {
+    item: $scope.items[0]
+  };
+
+  $scope.ok = function () {
+    $modalInstance.close($scope.selected.item);
+  };
+
+  $scope.cancel = function () {
+    $modalInstance.dismiss('cancel');
+  };
+});
